21bf0ed2215b91b6138a40739598574d0053be39
Tang Weng Kitt <wengkitt.tang@gmail.com>
2023-04-28 23:40:51 +0800

    Setup solidjs framework and add linter

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:solid/typescript"],
+  overrides: [],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["solid"],
+  rules: {},
+};
